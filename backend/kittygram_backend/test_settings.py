8fb658c2a721701e9b1801cdae98c9446e391c01
Gevork23 <gevork230702@mail.ru>
2025-12-12 19:04:29 +0300

    Update production compose with db environment and fix paths

--- backend/kittygram_backend/test_settings.py
+++ backend/kittygram_backend/test_settings.py
@@ -1,10 +1,12 @@
+# backend/kittygram_backend/test_settings.py
 from .settings import *
+import os
 
 # Используем SQLite для тестов
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -13,5 +15,6 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
 
-# Отключаем некоторые настройки для тестов
-DEBUG = False
+# Отключаем для тестов
+DEBUG = True
+SECRET_KEY = 'test-secret-key-for-ci'
